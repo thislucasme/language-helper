4aec624004215524e16a15c20ded82eaa2ba93fd
lucas <lucas@dias.lucas>
2024-06-08 19:45:08 -0300

    e

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Chat } from "../presentation/Chat";
-import { Box, Center, Text, VStack } from "@chakra-ui/react";
+import { Box, Center, Link, Text, VStack } from "@chakra-ui/react";
 
 export const App: React.FC = () => {
   return (
     <Box >
    <VStack w={"full"} h={"65px"} bgGradient="linear(to-r, white, blue, red)">
 <Center h={"65px"}>
+<Link href="https://www.instagram.com/thislucasme?igsh=MTZydHJwa213bzdpbw==" isExternal>
 <Text fontWeight={"bold"} fontSize={"large"} color={"white"}>Dev @thislucasme</Text>
+</Link>
 </Center>
       </VStack>
       <Box p={5}>
